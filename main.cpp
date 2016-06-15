25f063947809b16bd4e37dc671d2d6560bf94a20
zoumengchao <zoumengchao@163.com>
2016-06-15 18:11:31 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,11 +1,15 @@
 #include "stdio.h"
 #include "iostream"
+#include "opencv.hpp"
 
 using namespace std;
-
+using namespace cv;
 
 void main(){
 
+Mat test = imread("zmc.jpg")
+imshow("zmc",test);
+waitKey();
 cout<<"Hello world"<<endl;
 
 }
